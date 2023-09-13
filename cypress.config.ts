97f97854ac7fd134ccbfd9d9ab14b9ab88eb8436
Lena <lena@Lenas-MacBook-Pro.local>
2023-09-12 22:25:03 -0700

    change base url

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://www.cypress.io',
+    baseUrl: 'https://stackadapt-interview.s3.amazonaws.com',
 
   },
   retries: 1
